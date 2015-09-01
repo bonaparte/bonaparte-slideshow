a246bba90eb394191a920e953b1ad34da6374bde
LimH <hani.lim@dowjones.com>
2015-09-01 16:50:51 -0400

    bonaparte now gets from git

--- dist/bonaparte-slideshow.js
+++ dist/bonaparte-slideshow.js
@@ -78,10 +78,9 @@
 /* 4 */
 /***/ function(module, exports, __webpack_require__) {
 
-	/* WEBPACK VAR INJECTION */(function(__dirname, __filename) {var objct = __webpack_require__(5);
+	var objct = __webpack_require__(5);
 	var bp = __webpack_require__(7);
 
-	console.log("test", __dirname, __filename);
 	///////////////////////////////////////////////////////////////////////////////
 	// Polyfills
 
@@ -238,7 +237,6 @@
 	///////////////////////////////////////////////////////////////////////////////
 
 
-	/* WEBPACK VAR INJECTION */}.call(exports, "/", "/index.js"))
 
 /***/ },
 /* 5 */
